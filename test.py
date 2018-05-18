baf16a7c50f837ad2dc924cc9ab25cd67bf93020
Kostis Karantias <karantiaskostis@gmail.com>
2018-05-18 14:24:28 +0000

    test symbol table on exams testcase

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
-from compiler import (Argument, FunctionEntity, ParameterEntity, SymbolTable,
-                      VariableEntity)
+from compiler import (Argument, FunctionEntity, ParameterEntity, Scope,
+                      SymbolTable, TempVariableEntity, VariableEntity)
 
 
 class SymbolTableTest(unittest.TestCase):
@@ -36,6 +36,85 @@ class SymbolTableTest(unittest.TestCase):
             ParameterEntity(name="x", mode="cv", offset=12),
             ParameterEntity(name="y", mode="ref", offset=16)
         ])
+
+    def test_exam_example(self):
+        tbl = SymbolTable()
+        tbl.create_scope()
+        tbl.add_entity(VariableEntity("a"))
+        tbl.add_entity(VariableEntity("b"))
+        tbl.add_entity(FunctionEntity("P1", 3))
+        tbl.add_argument(Argument("x", "cv"))
+        tbl.add_argument(Argument("y", "ref"))
+
+        tbl.create_scope()
+        tbl.add_entity(VariableEntity("c"))
+        tbl.add_entity(VariableEntity("d"))
+        tbl.add_entity(FunctionEntity("P11", 6))
+        tbl.add_argument(Argument("w", "cv"))
+        tbl.add_argument(Argument("z", "ref"))
+
+        tbl.create_scope()
+        tbl.add_entity(VariableEntity("e"))
+        tbl.add_entity(FunctionEntity("P21", 8))
+        tbl.add_argument(Argument("x", "cv"))
+
+        tbl.create_scope()
+        tbl.add_entity(TempVariableEntity("T_0"))
+
+        self.assertEqual(len(tbl.scopes), 4)
+        self.assertEqual(
+            tbl.scopes[0],
+            Scope(
+                entities=[
+                    VariableEntity(name="a", offset=12),
+                    VariableEntity(name="b", offset=16),
+                    FunctionEntity(
+                        name="P1",
+                        start_quad=3,
+                        arguments=[Argument("x", "cv"),
+                                   Argument("y", "ref")])
+                ],
+                nesting_level=0))
+
+        self.assertEqual(
+            tbl.scopes[1],
+            Scope(
+                entities=[
+                    ParameterEntity(name="x", mode="cv", offset=12),
+                    ParameterEntity(name="y", mode="ref", offset=16),
+                    VariableEntity(name="c", offset=20),
+                    VariableEntity(name="d", offset=24),
+                    FunctionEntity(
+                        name="P11",
+                        start_quad=6,
+                        arguments=[Argument("w", "cv"),
+                                   Argument("z", "ref")])
+                ],
+                nesting_level=1))
+
+        self.assertEqual(
+            tbl.scopes[2],
+            Scope(
+                entities=[
+                    ParameterEntity(name="w", mode="cv", offset=12),
+                    ParameterEntity(name="z", mode="ref", offset=16),
+                    VariableEntity(name="e", offset=20),
+                    FunctionEntity(
+                        name="P21",
+                        start_quad=8,
+                        arguments=[Argument("x", "cv")])
+                    # TODO: add framelength (20) above ^
+                ],
+                nesting_level=2))
+
+        self.assertEqual(tbl.scopes[3],
+                         Scope(
+                             entities=[
+                                 ParameterEntity(
+                                     name="x", mode="cv", offset=12),
+                                 TempVariableEntity(name="T_0", offset=16)
+                             ],
+                             nesting_level=3))
 
 
 if __name__ == '__main__':
