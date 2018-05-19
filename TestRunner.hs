a9058e8d0ae772f3f3f6e43d8827ede91206b8d5
Kostis Karantias <karantiaskostis@gmail.com>
2018-05-19 17:09:53 +0000

    TestRunner: fix according to hlint

--- TestRunner.hs
+++ TestRunner.hs
@@ -1,10 +1,10 @@
 #!/usr/bin/env runhaskell
-import Control.Monad (filterM)
+import Control.Monad (filterM, unless)
 import System.Directory (getCurrentDirectory, listDirectory, removeDirectoryRecursive)
 import System.FilePath (hasExtension, takeExtension, replaceBaseName, takeBaseName, (</>), takeFileName)
 import Data.List (isPrefixOf, isInfixOf)
 import System.Process (createProcess, waitForProcess, shell, CreateProcess(..), StdStream(..))
-import System.Exit (ExitCode(..), exitWith)
+import System.Exit (ExitCode(..), exitWith, exitSuccess, exitFailure)
 import System.Posix.Temp (mkdtemp)
 import GHC.IO.Handle (hGetContents)
 import Data.Maybe (fromJust)
@@ -12,20 +12,20 @@ import Data.Maybe (fromJust)
 containsAnnotation :: String -> String -> Bool
 containsAnnotation annotation line = ("//" `isPrefixOf` line) && ("@" ++ annotation) `isInfixOf` line
 
-fileContainsAnnotation :: String -> FilePath -> IO (Bool)
+fileContainsAnnotation :: String -> FilePath -> IO Bool
 fileContainsAnnotation annotation file = do
     content <- readFile file
     let linesOfFile = lines content
     return $ any (containsAnnotation annotation) linesOfFile
 
-shouldCompile :: FilePath -> IO (Bool)
+shouldCompile :: FilePath -> IO Bool
 shouldCompile = fileContainsAnnotation "should_compile"
 
-shouldNotCompile :: FilePath -> IO (Bool)
+shouldNotCompile :: FilePath -> IO Bool
 shouldNotCompile = fileContainsAnnotation "should_not_compile"
 
 -- TODO(gtklocker): is this really the best way to accomplish this?
-shouldBeTested :: FilePath -> IO (Bool)
+shouldBeTested :: FilePath -> IO Bool
 shouldBeTested file = do
     should <- shouldCompile file
     shouldNot <- shouldNotCompile file
@@ -42,17 +42,15 @@ passesTest file = do
     let compiler = cwd </> "compiler.py"
     let cmd = unwords ["python3", compiler, file]
     tempDir <- mkdtemp "compiler-test"
-    (_, stdout, _, ph) <- createProcess $ (shell $ cmd){ cwd = Just tempDir, std_out = CreatePipe, std_err = CreatePipe }
+    (_, stdout, _, ph) <- createProcess $ (shell cmd){ cwd = Just tempDir, std_out = CreatePipe, std_err = CreatePipe }
     exit <- waitForProcess ph
     output <- hGetContents $ fromJust stdout
     removeDirectoryRecursive tempDir
     should <- shouldCompile file
     shouldNot <- shouldNotCompile file
     case exit of
-        ExitSuccess -> do
-            return (should, Nothing)
-        ExitFailure _ -> do
-            return (shouldNot, Just output)
+        ExitSuccess -> return (should, Nothing)
+        ExitFailure _ -> return (shouldNot, Just output)
 
 greenPutStrLn :: String -> IO ()
 greenPutStrLn output = putStrLn $ "\x1b[32m" ++ output ++ "\x1b[0m"
@@ -60,9 +58,9 @@ greenPutStrLn output = putStrLn $ "\x1b[32m" ++ output ++ "\x1b[0m"
 redPutStrLn :: String -> IO ()
 redPutStrLn output = putStrLn $ "\x1b[31m" ++ output ++ "\x1b[0m"
 
-checkFile :: FilePath -> IO (Bool)
+checkFile :: FilePath -> IO Bool
 checkFile file = do
-    putStr $ (takeFileName file) ++ "... "
+    putStr $ takeFileName file ++ "... "
     (passes, output) <- passesTest file
 
     if passes
@@ -70,20 +68,18 @@ checkFile file = do
     else redPutStrLn "fail"
 
     case output of 
-        Just out -> do
-            if not passes then putStrLn out else return ()
-        Nothing -> do
-            return ()
-    return $ passes
+        Just out -> unless passes $ putStrLn out
+        Nothing -> return ()
+    return passes
 
 main :: IO ()
 main = do
     currentDir <- getCurrentDirectory
     exampleFiles <- listDirectoryAbsolute $ currentDir </> "examples/"
 
-    let testFiles = filter (\filename -> takeExtension filename == ".eel") $ exampleFiles
+    let testFiles = filter (\filename -> takeExtension filename == ".eel") exampleFiles
     annotatedFiles <- filterM shouldBeTested testFiles
     oks <- mapM checkFile annotatedFiles
 
-    if all id oks then exitWith ExitSuccess
-    else exitWith $ ExitFailure 1
+    if and oks then exitSuccess
+    else exitFailure
